a9ab8452358d0ec41583db3815d1a2686bff9b7d
Thiago Matos <thiagoematos@gmail.com>
2018-12-12 23:41:56 -0400

    swagger para documentar a API

--- src/main/java/credere/prova/backend/BackendApplication.java
+++ src/main/java/credere/prova/backend/BackendApplication.java
@@ -2,12 +2,37 @@ package credere.prova.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class BackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApplication.class, args);
+    }
+
+    @Bean
+    public Docket spacialProbeApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("credere.prova.backend"))
+                .build()
+                .apiInfo(metaData());
+    }
+
+    private ApiInfo metaData() {
+        return new ApiInfoBuilder()
+                .title("Sonda Espacial REST API")
+                .description("\"Sonda Espacial REST API para controlar a posição da sonda.\"")
+                .version("1.0.0")
+                .build();
+    }
 
 }
--- src/main/java/credere/prova/backend/controller/SpacialProbeController.java
+++ src/main/java/credere/prova/backend/controller/SpacialProbeController.java
@@ -4,12 +4,15 @@ import credere.prova.backend.exception.InvalidMovementException;
 import credere.prova.backend.model.Movements;
 import credere.prova.backend.model.SpacialProbe;
 import credere.prova.backend.service.SpacialProbeService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@Api(value = "Posição", description = "Controlar a posição da sonda, através de movimentos pré-definidos.")
 @RestController
 @RequestMapping("/posicao")
 public class SpacialProbeController {
@@ -17,11 +20,13 @@ public class SpacialProbeController {
     @Autowired
     private SpacialProbeService service;
 
+    @ApiOperation(value = "Obtém a posição atual da sonda.")
     @GetMapping
     public SpacialProbe current() {
         return service.getCurrent();
     }
 
+    @ApiOperation(value = "Faz a sonda movimentar-se.")
     @PutMapping
     public void move(@RequestBody Movements movements, HttpServletResponse response) throws IOException {
         try {
@@ -31,6 +36,7 @@ public class SpacialProbeController {
         }
     }
 
+    @ApiOperation(value = "Volta a sonda para a posição inicial.")
     @DeleteMapping
     public void resetPosition() {
         service.resetPosition();
